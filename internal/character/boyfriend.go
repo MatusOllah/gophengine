c699af4b15c02621992fd10e0e703472dfa6af9c
MatusOllah <matusollah420@gmail.com>
2023-08-11 14:41:55 +0200

    Update boyfriend.go

--- internal/character/boyfriend.go
+++ internal/character/boyfriend.go
@@ -4,15 +4,21 @@ package character
 type Boyfriend struct {
 	*Character
 	IsStunned bool
+	IsGoat    bool
 }
 
 func NewBoyfriend(x, y float64) *Boyfriend {
 	return &Boyfriend{
 		Character: NewCharacter(x, y, "bf", true),
 		IsStunned: false,
+		IsGoat:    true,
 	}
 }
 
 func (bf *Boyfriend) Update(dt float64) error {
+	if !bf.IsGoat {
+		panic("(*Boyfriend).IsGoat must be true")
+	}
+
 	return nil
 }
