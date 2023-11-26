83c25625016300dfa7ba909dbff61d9e326db90a
Matúš Ollah <matusollah420@gmail.com>
2023-11-26 14:12:29 +0100

    Update state.go

--- internal/state/state.go
+++ internal/state/state.go
@@ -2,7 +2,15 @@ package state
 
 import "github.com/hajimehoshi/ebiten/v2"
 
-type State interface {
+type Updater interface {
 	Update(dt float64) error
+}
+
+type Drawer interface {
 	Draw(screen *ebiten.Image)
+}
+
+type State interface {
+	Updater
+	Drawer
 }
