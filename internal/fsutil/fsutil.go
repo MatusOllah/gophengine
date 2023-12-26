6d83b16de8c7a1bd6ce350b7d072654887e9851b
Matúš Ollah <matusollah420@gmail.com>
2023-12-26 16:11:02 +0100

    move filesystem extracting routine to fsutil

--- internal/fsutil/fsutil.go
+++ internal/fsutil/fsutil.go
@@ -1,6 +1,14 @@
 package fsutil
 
-import "io/fs"
+import (
+	"io"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+
+	"github.com/ncruces/zenity"
+)
 
 // NumFiles counts all the files in the filesystem and returns the number of files.
 func NumFiles(fsys fs.FS) (int, error) {
@@ -21,4 +29,92 @@ func NumFiles(fsys fs.FS) (int, error) {
 	}
 
 	return num, nil
+}
+
+// Extract extracts the filesystem to dst and shows progress dialog using zenity.ProgressDialog if gui is true.
+func Extract(fsys fs.FS, dst string, gui bool) error {
+	var value int = 0             // progress bar value
+	var dlg zenity.ProgressDialog // the progress bar dialog
+
+	// initialize progress bar dialog
+	if gui {
+		numFiles, err := NumFiles(fsys)
+		if err != nil {
+			return err
+		}
+
+		_dlg, err := zenity.Progress(zenity.Title("Extracting assets"), zenity.MaxValue(numFiles))
+		if err != nil {
+			return err
+		}
+		defer _dlg.Close()
+		dlg = _dlg
+
+		dlg.Text("Extracting...")
+	}
+
+	// create destination directory
+	if err := os.Mkdir(dst, fs.ModePerm); err != nil {
+		return err
+	}
+
+	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// create directory
+		if d.IsDir() {
+			dirPath := filepath.Join(dst, path)
+
+			if gui {
+				dlg.Text("Creating directory " + dirPath)
+			}
+
+			slog.Info("creating directory", "path", dirPath)
+			if err := os.MkdirAll(dirPath, fs.ModePerm); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		// create file
+		dstPath := filepath.Join(dst, path)
+
+		if gui {
+			value++
+			dlg.Value(value)
+			dlg.Text("extracting " + path)
+		}
+
+		slog.Info("extracting", "src", path, "dst", dstPath)
+
+		srcFile, err := fsys.Open(path)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+
+		dstFile, err := os.Create(dstPath)
+		if err != nil {
+			return err
+		}
+		defer dstFile.Close()
+
+		if _, err := io.Copy(dstFile, srcFile); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if gui {
+		dlg.Complete()
+	}
+
+	return nil
 }
--- internal/gophengine/extractAssets.go
+++ internal/gophengine/extractAssets.go
@@ -1,101 +1,11 @@
 package gophengine
 
 import (
-	"fmt"
-	"io"
-	"io/fs"
-	"log/slog"
-	"os"
-	"path/filepath"
-
 	"github.com/MatusOllah/gophengine/assets"
 	"github.com/MatusOllah/gophengine/internal/flagutil"
 	"github.com/MatusOllah/gophengine/internal/fsutil"
-	"github.com/ncruces/zenity"
 )
 
 func ExtractAssets() error {
-	isGUI := flagutil.MustGetBool(G.FlagSet, "gui")
-
-	var value int = 0
-	var dlg zenity.ProgressDialog
-	if isGUI {
-		slog.Info("counting files")
-		numFiles, err := fsutil.NumFiles(assets.FS)
-		if err != nil {
-			return err
-		}
-		slog.Info("done", "numFiles", numFiles)
-
-		_dlg, err := zenity.Progress(zenity.Title("Extracting assets"), zenity.MaxValue(numFiles))
-		if err != nil {
-			return err
-		}
-		defer _dlg.Close()
-		dlg = _dlg
-
-		dlg.Text("Extracting...")
-	}
-
-	if err := os.Mkdir("assets", fs.ModePerm); err != nil {
-		return err
-	}
-
-	err := fs.WalkDir(assets.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			dirPath := filepath.Join("assets", path)
-
-			if isGUI {
-				dlg.Text("Creating directory " + dirPath)
-			}
-
-			slog.Info(fmt.Sprintf("creating directory %s", dirPath))
-			if err := os.MkdirAll(dirPath, fs.ModePerm); err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		dstPath := filepath.Join("assets", path)
-
-		if isGUI {
-			value++
-			dlg.Value(value)
-			dlg.Text(fmt.Sprintf("extracting %s", path))
-		}
-
-		slog.Info(fmt.Sprintf("extracting %s => %s", path, dstPath))
-
-		src, err := assets.FS.Open(path)
-		if err != nil {
-			return err
-		}
-		defer src.Close()
-
-		dst, err := os.Create(dstPath)
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
-
-		if _, err := io.Copy(dst, src); err != nil {
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	if isGUI {
-		dlg.Complete()
-	}
-
-	return nil
+	return fsutil.Extract(assets.FS, "assets", flagutil.MustGetBool(G.FlagSet, "gui"))
 }
