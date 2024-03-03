d5e844ad2bcff3bd339f78be49329ff2a3ba2b0f
Matúš Ollah <matusollah420@gmail.com>
2024-03-03 17:23:46 +0100

    Update doc.go

--- internal/character/boyfriend/doc.go
+++ internal/character/boyfriend/doc.go
@@ -1,2 +1,2 @@
-// Package boyfriend implements Boyfriend as defined here: https://github.com/FunkinCrew/Funkin/blob/master/source/Boyfriend.hx
+// Package boyfriend implements Boyfriend as defined here: https://github.com/FunkinCrew/Funkin/blob/v0.2.7.1/source/Boyfriend.hx
 package boyfriend
