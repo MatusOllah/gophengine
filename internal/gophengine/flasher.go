4c11e0c168c7fc8b630f597b26c668beae185e66
Matúš Ollah <matusollah420@gmail.com>
2024-04-26 21:10:42 +0200

    improve flasher

--- internal/gophengine/flasher.go
+++ internal/gophengine/flasher.go
@@ -1,40 +1,33 @@
 package gophengine
 
 import (
+	"image/color"
 	"log/slog"
 
-	"github.com/MatusOllah/gophengine/assets"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/colorm"
 	"github.com/tanema/gween"
 	"github.com/tanema/gween/ease"
 )
 
 type Flasher struct {
-	shader *ebiten.Shader
-	alpha  float32
-	flash  bool
-	done   bool
-	tween  *gween.Tween
+	alpha    float32
+	flash    bool
+	done     bool
+	tween    *gween.Tween
+	whiteImg *ebiten.Image
 }
 
 func NewFlasher(dur float32) (*Flasher, error) {
-	slog.Info("compiling shaders")
-	b, err := assets.FS.ReadFile("data/shaders/flash.kage")
-	if err != nil {
-		return nil, err
-	}
-
-	shader, err := ebiten.NewShader(b)
-	if err != nil {
-		return nil, err
-	}
+	white := ebiten.NewImage(G.Width, G.Height)
+	white.Fill(color.White)
 
 	return &Flasher{
-		shader: shader,
-		alpha:  1,
-		flash:  false,
-		done:   false,
-		tween:  gween.New(1, 0, dur, ease.Linear),
+		alpha:    1,
+		flash:    false,
+		done:     false,
+		tween:    gween.New(1, 0, dur, ease.Linear),
+		whiteImg: white,
 	}, nil
 }
 
@@ -42,16 +35,9 @@ func (f *Flasher) Draw(img *ebiten.Image) {
 	if f.flash && !f.done {
 		slog.Debug("drawing", "flash", f.flash, "done", f.done, "alpha", f.alpha)
 
-		newImg := ebiten.NewImage(img.Bounds().Dx(), img.Bounds().Dy())
-
-		op := &ebiten.DrawRectShaderOptions{}
-		op.Images[0] = img
-		op.Uniforms = map[string]any{
-			"Opacity": f.alpha,
-		}
-		newImg.DrawRectShader(newImg.Bounds().Dx(), newImg.Bounds().Dy(), f.shader, op)
-
-		img.DrawImage(newImg, &ebiten.DrawImageOptions{})
+		cm := colorm.ColorM{}
+		cm.Scale(255, 255, 255, float64(f.alpha))
+		colorm.DrawImage(img, f.whiteImg, cm, &colorm.DrawImageOptions{})
 	}
 }
 
@@ -71,5 +57,6 @@ func (f *Flasher) Update(dt float64) error {
 }
 
 func (f *Flasher) Flash() {
+	f.tween.Reset()
 	f.flash = true
 }
