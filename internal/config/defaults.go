d13a3beb1da0376fba83ed4bc796528e0e110318
Matúš Ollah <matusollah420@gmail.com>
2024-04-30 22:08:02 +0200

    Update defaults.go

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -1,12 +1,19 @@
 package config
 
-import "github.com/jeandeaual/go-locale"
+import (
+	"log/slog"
+
+	"github.com/jeandeaual/go-locale"
+)
 
 func LoadDefaultOptions(cfg *Config) {
+	slog.Info("config: loading defaults")
+
 	userLocale, err := locale.GetLocale()
 	if err != nil {
 		panic(err)
 	}
+	slog.Info("got locale", "userLocale", userLocale)
 
 	cfg.SetData(map[string]interface{}{
 		"Locale": userLocale,
