f7a7aefba20690ab24513004dce3867beb643007
Matúš Ollah <matusollah420@gmail.com>
2024-09-06 22:04:37 +0200

    comment Scene

--- scene.go
+++ scene.go
@@ -2,9 +2,17 @@ package gophengine
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+// Scene represents a game scene.
 type Scene interface {
+	// Init initializes the scene. It's called once before the scene is displayed.
 	Init() error
+
+	// Close cleans up resources and closes the scene.
 	Close() error
+
+	// Update updates the scene by one tick; dt is the time since the last update (aka delta time).
 	Update(dt float64) error
+
+	// Draw renders the scene onto the screen for the current frame.
 	Draw(screen *ebiten.Image)
 }
