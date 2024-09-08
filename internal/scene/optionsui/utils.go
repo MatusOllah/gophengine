e4d246c6c49c7e1a2b9dc8c3196eaa99e3b8564f
Matúš Ollah <matusollah420@gmail.com>
2024-09-08 20:19:14 +0200

    add comment

--- internal/scene/optionsui/utils.go
+++ internal/scene/optionsui/utils.go
@@ -11,6 +11,7 @@ func newLabelColorSimple(clr color.Color) *widget.LabelColor {
 	return &widget.LabelColor{clr, clr}
 }
 
+// newButtonTextColorSimple is short for &widget.ButtonTextColor{clr, clr, clr, clr}.
 func newButtonTextColorSimple(clr color.Color) *widget.ButtonTextColor {
 	return &widget.ButtonTextColor{clr, clr, clr, clr}
 }
