c6e63870225c7481f8ff75aa266b5b705582faef
Matúš Ollah <matusollah420@gmail.com>
2024-11-09 21:55:36 +0100

    comment OpenURL

--- internal/browser/browser_desktop.go
+++ internal/browser/browser_desktop.go
@@ -4,6 +4,7 @@ package browser
 
 import pkg_browser "github.com/pkg/browser"
 
+// OpenURL opens a new browser window pointing to url.
 func OpenURL(url string) error {
 	return pkg_browser.OpenURL(url)
 }
--- internal/browser/browser_js.go
+++ internal/browser/browser_js.go
@@ -4,6 +4,7 @@ package browser
 
 import "syscall/js"
 
+// OpenURL opens a new browser window pointing to url.
 func OpenURL(url string) error {
 	js.Global().Get("window").Get("open").Invoke(url)
 	return nil
