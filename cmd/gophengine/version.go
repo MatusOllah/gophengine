f8882a1dee60580d665b707aeb5f6701181c318b
Matúš Ollah <matusollah420@gmail.com>
2024-12-29 13:40:38 +0100

    Update version.go

--- cmd/gophengine/version.go
+++ cmd/gophengine/version.go
@@ -1,7 +1,7 @@
 package main
 
 // version represents the GophEngine version.
-const version string = "1.0.0"
+const version string = "0.0.0-devel"
 
 /*
 fnfVersion represents the Friday Night Funkin' game version. This should always remain "0.2.7.1" as GophEngine is based on FNF v0.2.7.1.
