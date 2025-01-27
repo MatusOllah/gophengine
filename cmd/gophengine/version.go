65bd33468faf84f611fc62bf546896fc9dfdbd32
Matúš Ollah <matusollah420@gmail.com>
2025-01-27 08:17:52 +0100

    Update version.go

--- cmd/gophengine/version.go
+++ cmd/gophengine/version.go
@@ -3,11 +3,5 @@ package main
 // version represents the GophEngine version.
 const version string = "0.0.0-devel"
 
-/*
-fnfVersion represents the Friday Night Funkin' game version. This should always remain "0.2.7.1" as GophEngine is based on FNF v0.2.7.1.
-
-This version was chosen because it's the last "normal" version before NSFW & violent cutscenes (introduced in FNF v0.3.2), which conflicts with the project's goals.
-I strongly oppose this kind of content and behavior and want to create a family-friendly alternative, as it can be distressing for many, including myself.
-Additionally, FNF v0.2.7.1 is stable and well-supported by the modding community and third-party modding engines (e.g., Kade Engine, Psych Engine).
-*/
+// fnfVersion represents the Friday Night Funkin' game version. This should always remain "0.2.7.1" as GophEngine is based on FNF v0.2.7.1.
 const fnfVersion string = "0.2.7.1"
