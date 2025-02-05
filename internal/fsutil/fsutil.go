126500baa22d33029722f7e728730c67e9ce58d8
Matúš Ollah <matusollah420@gmail.com>
2025-02-05 10:34:31 +0100

    remove unused function

--- internal/fsutil/fsutil.go
+++ internal/fsutil/fsutil.go
@@ -8,30 +8,8 @@ import (
 	"path/filepath"
 )
 
-// NumFiles counts all the files in the filesystem and returns the number of files.
-func NumFiles(fsys fs.FS) (int, error) {
-	var num int
-	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
-
-		num++
-		return nil
-	})
-	if err != nil {
-		return 0, err
-	}
-
-	return num, nil
-}
-
 // Extract extracts the filesystem to dst.
 func Extract(fsys fs.FS, dst string) error {
-
 	// create destination directory
 	if err := os.Mkdir(dst, fs.ModePerm); err != nil {
 		return err
