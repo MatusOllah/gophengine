1c8b7ff61df896d5f7e38ec4f0802d92a8299bfd
Matúš Ollah <matusollah420@gmail.com>
2025-03-20 10:02:40 +0100

    context: make week name cases consistent

--- context/weeks.go
+++ context/weeks.go
@@ -7,9 +7,9 @@ func initWeeks() []funkin.Week {
 		{Name: "", ID: "tutorial", Songs: []string{"tutorial"}, MenuCharacters: []string{"dad", "bf", "gf"}},
 		{Name: "Daddy Dearest", ID: "week1", Songs: []string{"bopeebo", "fresh", "dadbattle"}, MenuCharacters: []string{"dad", "bf", "gf"}},
 		{Name: "Spooky Month", ID: "week2", Songs: []string{"spookeez", "south"}, MenuCharacters: []string{"spooky", "bf", "gf"}},
-		{Name: "PICO", ID: "week3", Songs: []string{"pico", "philly", "blammed"}, MenuCharacters: []string{"pico", "bf", "gf"}},
-		{Name: "MOMMY MEAREST", ID: "week4", Songs: []string{"satin-panties", "high", "milf"}, MenuCharacters: []string{"mom", "bf", "gf"}},
+		{Name: "Pico", ID: "week3", Songs: []string{"pico", "philly", "blammed"}, MenuCharacters: []string{"pico", "bf", "gf"}},
+		{Name: "Mommy Mearest", ID: "week4", Songs: []string{"satin-panties", "high", "milf"}, MenuCharacters: []string{"mom", "bf", "gf"}},
 		{Name: "Happy and Merry", ID: "week5", Songs: []string{"cocoa", "eggnog"}, MenuCharacters: []string{"parents-christmas", "bf", "gf"}},
-		{Name: "dating simulator ft. moawling", ID: "week6", Songs: []string{"senpai", "roses", "thorns"}, MenuCharacters: []string{"senpai", "bf", "gf"}},
+		{Name: "Dating simulator ft. moawling", ID: "week6", Songs: []string{"senpai", "roses", "thorns"}, MenuCharacters: []string{"senpai", "bf", "gf"}},
 	}
 }
